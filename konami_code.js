4d529680252e7346accaead1351e5f591380a4ca
James Ellis <13jellis@toothillschool.co.uk>
2019-07-17 08:27:56 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,18 +12,22 @@ const codes = [
 ];
 
 function init() {
-  var index = 0;
-  function onKeyDownHandler(e) {
-    const key = e.key;
-    if (key === codes[index]) {
-      index++;
-      if (index === codes.length) {
-        alert("Wow!");
-        index = 0;
-      }
-    }
-    else {
+  document.body.addEventListener('keydown')
+}
+
+init()
+
+var index = 0;
+function onKeyDownHandler(e) {
+  const key = e.key;
+  if (key === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      alert("Wow!");
       index = 0;
     }
+  }
+  else {
+    index = 0;
   }
 }
